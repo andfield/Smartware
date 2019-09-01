2d6f2b52901ead24f911e57b716e40b2f23c6c97
Conor-Oliver <conor.oliver16@gmail.com>
2019-09-01 23:02:27 +1200

    installed firebase and added routes to router,js

--- src/main.js
+++ src/main.js
@@ -1,9 +1,21 @@
 import Vue from 'vue'
+import firebase from 'firebase'
 import App from './App.vue'
 import router from './router'
 import vuetify from './plugins/vuetify';
 
 Vue.config.productionTip = false
+
+var config = {
+  apiKey: "AIzaSyBpyAw6R0OYtsBZEXk-yCM7awcP9VvL2S8",
+  authDomain: "smartware-website-project.firebaseapp.com",
+  databaseURL: "https://smartware-website-project.firebaseio.com",
+  projectId: "smartware-website-project",
+  storageBucket: "smartware-website-project.appspot.com",
+  messagingSenderId: "218476766597",
+  appId: "1:218476766597:web:f1f617802dd2a710"
+};
+firebase.initializeApp(config);
 
 new Vue({
   router,
--- src/router.js
+++ src/router.js
@@ -3,20 +3,34 @@ import Router from 'vue-router'
 
 import Home from './views/Home.vue'
 import About from './views/About.vue'
+import LandingPage from './views/LandingPage.vue'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
+      path: '*',
+      redirect: '/login'
+    },
+    {
       path: '/',
-      name: 'home',
-      component: Home
+      redirect: '/login'
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: LandingPage
     },
     {
       path: '/about',
       name: 'about',
       component: About
+    },
+    {
+      path: '/home',
+      name: 'home',
+      component: Home
     }
   ]
 })
