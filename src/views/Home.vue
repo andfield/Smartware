c1e2b5253cc815b7874890a031489f6ae9a82ab9
Sid <siddharththakur76@gmail.com>
2019-09-09 20:48:00 +1200

    added some props

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -12,45 +12,8 @@
       </v-layout>
     </v-container>
 
-    <v-container>
-      <v-layout row wrap class="px-5">
-        <v-flex class="justify-center">
-          <h1>Printers</h1>
-          <v-divider></v-divider>
-        </v-flex>
-      </v-layout>
-    </v-container>
-
-    <v-container>
-      <v-layout row wrap class="mx-5">
-        <v-flex xs10 md4 lg2 xl2 v-for="product in products" :key="product.name">
-          <v-card class="ma-2" hover to="/Product">
-            <v-img
-              :src="product.imgURL"
-              lazy-src
-              class="black--text"
-              height="200px"
-              gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
-            >
-              <v-card-title class="fill-height align-end" v-text="product.name"></v-card-title>
-            </v-img>
-
-            <v-card-text>
-              <span v-text="product.description" />
-            </v-card-text>
-
-            <v-card-actions>
-              <v-icon>attach_money</v-icon>
-              <span v-text="product.stanPrice" />
-              <v-spacer />
-              <v-btn color="blue" dark small absolute bottom right fab>
-                <v-icon>add_shopping_cart</v-icon>
-              </v-btn>
-            </v-card-actions>
-          </v-card>
-        </v-flex>
-      </v-layout>
-    </v-container>
+    <productlist v-bind:products="products"/>
+    
     <v-container>
       <v-layout class="justify-center">
         <v-flex xs12 lg10 xl10>
@@ -58,6 +21,7 @@
         </v-flex>
       </v-layout>
     </v-container>
+
     <AppFooter />
   </div>
 </template>
@@ -69,29 +33,29 @@ import Carousel from "../components/Carousel";
 import NavFooter from "../components/NavFooter";
 import firebase, { functions, firestore } from "firebase";
 import db from "@/main";
-
-var exName = "new printer";
+import productlist from '../components/productlist.vue'
 
 export default {
   components: {
     AppToolbar,
     AppFooter,
     Carousel,
-    NavFooter
+    NavFooter,
+    productlist
   },
   data() {
-    return{
+    return {
       products: {},
       ProductCategory: {}
-    }
-    
+    };
   },
-  firestore(){
-    return{
-      products:  db.collection("ProductCategory")
-      .doc("pcQhnNHvSg1L3LYyTFdX")
-      .collection("EftposMachines"),
-    }
-  },
+  firestore() {
+    return {
+      products: db
+        .collection("ProductCategory")
+        .doc("pcQhnNHvSg1L3LYyTFdX")
+        .collection("EftposMachines")
+    };
+  }
 };
 </script>
--- src/views/LandingPage.vue
+++ src/views/LandingPage.vue
@@ -7,7 +7,7 @@
             <v-row>
               <v-col cols="12">
                 <v-row class="align-center justify-center">
-                  <h1 class="text-justify">
+                  <h1 class="text-justify display-2">
                     Welecome to
                     <strong>Smartware</strong>
                   </h1>
@@ -22,15 +22,23 @@
             </v-row>
             <v-row row wrap class="my-10">
               <v-col cols="5" class="mx-auto">
-                <v-card outlined max-width="500" class="mx-auto">
+                <v-card text hover max-width="500" class="mx-auto">
                   <v-card-title>
-                    <h3>Login to our site</h3>
-                    <p>Enter username and password to log on:</p>
+                    <v-list-item>
+                      <v-list-item-content>
+                        <v-list-item-title class="display-1">Login to our site</v-list-item-title>
+                        <v-list-item-subtitle
+                          class="hidden-md-and-down"
+                        >Enter username and password to log on:</v-list-item-subtitle>
+                      </v-list-item-content>
+                      <v-list-item-icon>
+                        <v-icon x-large>account_box</v-icon>
+                      </v-list-item-icon>
+                    </v-list-item>
                   </v-card-title>
-                  <v-card-actions>
+                  <v-card-text>
                     <v-text-field label="Email" v-model="loginEmail" outlined />
-                  </v-card-actions>
-                  <v-card-actions>
+
                     <v-text-field
                       v-model="loginPass"
                       :append-icon="show1 ? 'visibility' : 'visibility_off'"
@@ -43,7 +51,7 @@
                       outlined
                       @click:append="show1 = !show1"
                     ></v-text-field>
-                  </v-card-actions>
+                  </v-card-text>
                   <v-card-actions>
                     <v-btn block @click="login" color="blue" dark>Sign in</v-btn>
                   </v-card-actions>
@@ -55,22 +63,25 @@
               <v-col cols="5" class="mx-auto">
                 <v-card outlined max-width="600" class="mx-auto">
                   <v-card-title>
-                    <h3>Sign up now</h3>
-                    <p>Fill in the form below to get instant access:</p>
+                    <v-list-item>
+                      <v-list-item-content>
+                        <v-list-item-title class="display-1">SignUp to our site</v-list-item-title>
+                        <v-list-item-subtitle class="hidden-md-and-down">Fill in the form below</v-list-item-subtitle>
+                      </v-list-item-content>
+                      <v-list-item-icon>
+                        <v-icon x-large>create</v-icon>
+                      </v-list-item-icon>
+                    </v-list-item>
                   </v-card-title>
-                  <v-card-actions>
+                  <v-card-text>
                     <v-text-field label="First Name" v-model="fname" outlined />
-                  </v-card-actions>
-                  <v-card-actions>
+
                     <v-text-field label="Last Name" v-model="lname" outlined />
-                  </v-card-actions>
-                  <v-card-actions>
+
                     <v-text-field label="Email" v-model="email" outlined />
-                  </v-card-actions>
-                  <v-card-actions>
+
                     <v-text-field label="Phone No" v-model="pnum" outlined />
-                  </v-card-actions>
-                  <v-card-actions>
+
                     <v-text-field
                       v-model="pass"
                       :append-icon="show2 ? 'visibility' : 'visibility_off'"
@@ -83,7 +94,7 @@
                       outlined
                       @click:append="show2 = !show2"
                     ></v-text-field>
-                  </v-card-actions>
+                  </v-card-text>
                   <v-card-actions>
                     <v-btn @click="signUp" block color="blue" dark>Sign me up!!</v-btn>
                   </v-card-actions>
@@ -98,61 +109,69 @@
 </template>
 
 <script>
-import firebase, { functions } from 'firebase';
-import db from '@/main'
+import firebase, { functions } from "firebase";
+import db from "@/main";
 
 export default {
-    data() {
-		return{
-			show1: false,
-			show2: false,
-		 	rules: {
-				required: value => !!value || 'Required.',
-				min: v => v.length >= 8 || 'Min 8 characters',
-			},
-			fname: '',
-			lname: '',
-			email: '',
-			pnum: '',
-			pass: '',
-			loginEmail: '',
-			loginPass: '',
-		}
-	},
-	methods: {
-		signUp: function() {
-			firebase.auth().createUserWithEmailAndPassword(this.email, this.pass).then(
-				(user) => {
-					// this.$router.replace('home')
-				},
-				(err) => {
-					alert('An error has occured: ' + err.message)
-				}
-			);
-			var custData = {
-				email: this.email,
-				fName: this.fname,
-				lName: this.lname,
-				phNum: this.pnum
-			};
-			db.collection('customers').add(custData).then( this.$router.replace('home') );
-		},
-		login: function() {
-			firebase.auth().signInWithEmailAndPassword(this.loginEmail, this.loginPass).then(
-				(user) => {
-					this.$router.replace('home')
-				},
-				function(err) {
-					alert('Sign in failed. ' + err.message)
-				}
-			);
-		}
-	}
-}
+  data() {
+    return {
+      show1: false,
+      show2: false,
+      rules: {
+        required: value => !!value || "Required.",
+        min: v => v.length >= 8 || "Min 8 characters"
+      },
+      fname: "",
+      lname: "",
+      email: "",
+      pnum: "",
+      pass: "",
+      loginEmail: "",
+      loginPass: ""
+    };
+  },
+  methods: {
+    signUp: function() {
+      firebase
+        .auth()
+        .createUserWithEmailAndPassword(this.email, this.pass)
+        .then(
+          user => {
+            // this.$router.replace('home')
+          },
+          err => {
+            alert("An error has occured: " + err.message);
+          }
+        );
+      var custData = {
+        email: this.email,
+        fName: this.fname,
+        lName: this.lname,
+        phNum: this.pnum
+      };
+      db.collection("customers")
+        .add(custData)
+        .then(this.$router.replace("home"));
+    },
+    login: function() {
+      firebase
+        .auth()
+        .signInWithEmailAndPassword(this.loginEmail, this.loginPass)
+        .then(
+          user => {
+            this.$router.replace("home");
+          },
+          function(err) {
+            alert("Sign in failed. " + err.message);
+          }
+        );
+    }
+  }
+};
 </script>
 
 <style scoped>
-.back{
+.back {
   background-image: url("https://mdbootstrap.com/img/Photos/Horizontal/Nature/full page/img(11).jpg");
   background-size: cover;
   width: 100%;
