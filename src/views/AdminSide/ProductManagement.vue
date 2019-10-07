a3660daea8894f2562ac1ef022b9d395bed1dac6
Sid <siddharththakur76@gmail.com>
2019-10-08 10:35:50 +1300

    Product management done

--- src/views/AdminSide/ProductManagement.vue
+++ src/views/AdminSide/ProductManagement.vue
@@ -5,66 +5,38 @@
       <v-layout align-center justify-center>
         <v-flex xs12 sm12 md8 lg8 ls8>
           <v-card hover>
-            <v-toolbar dark>
-              <v-toolbar-title class="display-2">All Products</v-toolbar-title>
-              <v-spacer />
-              <v-text-field
-                v-model="search"
-                append-icon="search"
-                label="Search"
-                single-line
-                hide-details
-              />
-            </v-toolbar>
+            <v-card-text>
+              <v-data-table
+                :headers="headers"
+                :items="products"
+                sort-by="name"
+                class="elevation-1"
+                :search="search"
+              >
+                <template v-slot:top>
+                  <v-toolbar flat dark>
+                    <v-toolbar-title>Product Management</v-toolbar-title>
+                    <v-spacer />
+                    <v-text-field
+                      v-model="search"
+                      append-icon="search"
+                      label="Search"
+                      single-line
+                      hide-details
+                    ></v-text-field>
+                  </v-toolbar>
+                </template>
+                <template v-slot:item.actions="{ item }">
+                  <v-icon
+                    class="mr-2"
+                    color="primary"
+                    @click="$router.push({path: '/UpdateProduct', query: { currentProduct: item} })"
+                  >edit</v-icon>
 
-            <v-simple-table  height="200px" fixed-header>
-              <thead>
-                <th class="text-left hidden-md-and-down">Image</th>
-                <th>Name</th>
-                <th>Description</th>
-                <th>price</th>
-                <th>Controls</th>
-              </thead>
-              <tbody>
-                <tr v-for="(item) in products" :key="item.name"><!-- there is no loop counter -->
-                  <td class="hidden-md-and-down">
-                    <v-img :src="item.imgURL" max-height="100px" max-width="100px" />
-                  </td>
-                  <td>{{item.name}}</td>
-                  <td>{{item.description}}</td>
-                  <td>
-                    <span>
-                      <h3>${{parseFloat(item.stanPrice).toFixed(2)}}</h3>
-                    </span>
-                  </td>
-                  <td>
-                    <span>
-                      <v-row>
-                        <v-btn
-                          fab
-                          dark
-                          small
-                          color="primary"
-                          @click="$router.push({path: '/UpdateProduct', query: { currentProduct: item} })"
-                        >
-                          <v-icon>edit</v-icon>
-                        </v-btn>
-                        <v-btn
-                          class="mx-3"
-                          fab
-                          dark
-                          small
-                          color="error"
-                          @click="deleteProduct(item.id)"
-                        >
-                          <v-icon>delete</v-icon>
-                        </v-btn>
-                      </v-row>
-                    </span>
-                  </td>
-                </tr>
-              </tbody>
-            </v-simple-table>
+                  <v-icon color="error" @click="deleteProduct(item.id)">delete</v-icon>
+                </template>
+              </v-data-table>
+            </v-card-text>
             <v-card-actions>
               <v-btn small absolute bottom left color="success" fab to="/AddProduct">
                 <v-icon>add</v-icon>
@@ -74,25 +46,45 @@
         </v-flex>
       </v-layout>
     </v-container>
-    <v-btn @click="tempTest()">
-      yolo
-    </v-btn>
+    <v-btn @click="tempTest()">yolo</v-btn>
   </span>
 </template>
 
 <script>
 import firebase, { functions, firestore } from "firebase";
 import db from "@/main";
-import AdminToolBar from '@/components/AdminToolBar';
+import AdminToolBar from "@/components/AdminToolBar";
 export default {
   name: "ProductManagment",
   components: {
-      AdminToolBar,
+    AdminToolBar
   },
   data() {
     return {
       products: [],
-      search: ""
+      search: "",
+      headers: [
+        {
+          text: "Name",
+          align: "left",
+          sortable: true,
+          value: "name"
+        },
+        {
+          text: "Description",
+          value: "description"
+        },
+        {
+          text: "$ Price",
+          value: "stanPrice",
+          sortable: true
+        },
+        {
+          text: "Controls",
+          value: "actions",
+          sortable: false
+        }
+      ]
     };
   },
   firestore() {
@@ -101,8 +93,8 @@ export default {
         .collection("ProductCategory")
         .doc("vQzSkBnxzbu1Tnqn4iv2")
         .collection("Cables") // need to fix this
-    }
-    console.log("testing")
+    };
+    console.log("testing");
   },
   methods: {
     deleteProduct(item) {
@@ -115,16 +107,18 @@ export default {
       //   console.error("Error removing document: ", error);
       // });
     },
-    tempTest(){
-      console.log(this.products) 
-      var prodRef = db.collection("ProductCategory").doc("vQzSkBnxzbu1Tnqn4iv2");
+    tempTest() {
+      console.log(this.products);
+      var prodRef = db
+        .collection("ProductCategory")
+        .doc("vQzSkBnxzbu1Tnqn4iv2");
       prodRef.getCollections().then(collections => {
         collections.forEach(collection => {
-          console.log('Found subcollection with id:', collection.id);
+          console.log("Found subcollection with id:", collection.id);
         });
       });
     }
   }
-}
+};
 </script>
 
