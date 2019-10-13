f36efdd1dae6ee3918951d793f01dd36e0991eb5
Conor-Oliver <conor.oliver16@gmail.com>
2019-10-13 20:24:41 +1300

    password changes works

--- src/views/CustomerAccount.vue
+++ src/views/CustomerAccount.vue
@@ -113,7 +113,7 @@ export default {
       }
 
       if(this.newPass == this.newPassCon){
-        if (this.passLength >= 8 || this.newPass == null) {
+        if (this.passLength >= 8 || this.newPass != null) {
             this.$store.dispatch("updateCustomer", {
             newPhNum: this.newPhNum,
             newEmail: this.newEmail,
@@ -122,6 +122,7 @@ export default {
           });
         }
         else {
+          console.log(this.newPass.length)
           alert("The password is too short")
         }
       }
